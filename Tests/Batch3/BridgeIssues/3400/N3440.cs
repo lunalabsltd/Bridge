fe63a16e28e2ff7d9dfc72a698f618d7690654f3
Fabrício Zimmerer Murta <fabricio@object.net>
2018-03-19 15:06:57 -0300

    Adds comments to issue 3440's unit tests.

--- Tests/Batch3/BridgeIssues/3400/N3440.cs
+++ Tests/Batch3/BridgeIssues/3400/N3440.cs
@@ -3,10 +3,18 @@ using System;
 
 namespace Bridge.ClientTest.Batch3.BridgeIssues
 {
+    /// <summary>
+    /// The test here consists in ensuring overrides to ToString() method
+    /// works in some situations.
+    /// </summary>
     [Category(Constants.MODULE_ISSUES)]
     [TestFixture(TestNameFormat = "#3440 - {0}")]
     public class Bridge3440
     {
+        /// <summary>
+        /// This is a simple class with no convention, overriding the
+        /// ToString() method.
+        /// </summary>
         public class Class1
         {
             public override string ToString()
@@ -15,6 +23,10 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
             }
         }
 
+        /// <summary>
+        /// A class implementing the convention attribute and similarly
+        /// overriding the ToString() method.
+        /// </summary>
         [Convention]
         public class Class2
         {
@@ -24,20 +36,24 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
             }
         }
 
+        /// <summary>
+        /// Test the custom classes above and also System.IO's
+        /// [String/Text]Writer's ToString() call.
+        /// </summary>
         [Test]
         public static void TestRulesForOverride()
         {
             System.IO.TextWriter writer = new System.IO.StringWriter();
-            Assert.AreEqual("", writer.ToString());
+            Assert.AreEqual("", writer.ToString(), "Casting to TextWriter works.");
 
             System.IO.StringWriter writer1 = new System.IO.StringWriter();
-            Assert.AreEqual("", writer1.ToString());
+            Assert.AreEqual("", writer1.ToString(), "Without cast works.");
 
             object o = new Class1();
-            Assert.AreEqual("test1", o.ToString());
+            Assert.AreEqual("test1", o.ToString(), "Conventionless class' ToString() works.");
 
             o = new Class2();
-            Assert.AreEqual("test2", o.ToString());
+            Assert.AreEqual("test2", o.ToString(), "Convention-specified class ToString() works.");
         }
     }
 }
