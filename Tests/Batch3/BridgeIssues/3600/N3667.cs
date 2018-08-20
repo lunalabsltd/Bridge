71e518d42e9a26e73f183ffaf365ad90f592178e
Fabrício Zimmerer Murta <fabricio@object.net>
2018-08-20 15:39:31 -0300

    Adds comments to unit tests for issue 3667.

--- Tests/Batch3/BridgeIssues/3600/N3667.cs
+++ Tests/Batch3/BridgeIssues/3600/N3667.cs
@@ -1,11 +1,13 @@
-using Bridge.Html5;
 using Bridge.Test.NUnit;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Bridge.ClientTest.Batch3.BridgeIssues
 {
+    /// <summary>
+    /// Ensures fetching nullable variable value with the null conditional operator works.
+    /// </summary>
+    /// <remarks>
+    /// The operator is documented at: https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/null-conditional-operators
+    /// </remarks>
     [TestFixture(TestNameFormat = "#3667 - {0}")]
     public class Bridge3667
     {
@@ -14,8 +16,8 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
         {
             (string Prop1, string Prop2)? val = ("test1", "test2");
 
-            Assert.AreEqual("test1", val.Value.Prop1);
-            Assert.AreEqual("test1", val?.Prop1);
+            Assert.AreEqual("test1", val.Value.Prop1, "Fetching value via the nullable's .Value property works.");
+            Assert.AreEqual("test1", val?.Prop1, "Fetching value using the the null conditional operator works.");
         }
     }
 }
