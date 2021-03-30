542142a731f953c8b4b27feda489fdfac33fb076
Dmitry Abramenkov <dmitry.abramenkov@lunalabs.uk>
2021-03-31 00:50:41 +0300

    Styiling code to match c#.

--- Bridge/shared/System/Collections/DictionaryEntry.cs
+++ Bridge/shared/System/Collections/DictionaryEntry.cs
@@ -18,38 +18,28 @@ namespace System.Collections {
     using System;
     // A DictionaryEntry holds a key and a value from a dictionary.
     // It is returned by IDictionaryEnumerator::GetEntry().
-[System.Runtime.InteropServices.ComVisible(true)]
+    [Runtime.InteropServices.ComVisible(true)]
     [Serializable]
     public struct DictionaryEntry
     {
-        private Object _key;
-        private Object _value;
+        private object _key;
+        private object _value;
 
         // Constructs a new DictionaryEnumerator by setting the Key
         // and Value fields appropriately.
-        public DictionaryEntry(Object key, Object value) {
+        public DictionaryEntry(object key, object value) {
             _key = key;
             _value = value;
         }
 
-        public Object Key {
-            get {
-                return _key;
-            }
-
-            set {
-                _key = value;
-            }
+        public object Key {
+            get => _key;
+            set => _key = value;
         }
 
-        public Object Value {
-            get {
-                return _value;
-            }
-
-            set {
-                _value = value;
-            }
+        public object Value {
+            get => _value;
+            set => _value = value;
         }
     }
 }
